bfbe5846579ff06bb7274b0b2d79a1b891c91b5d
juanjo52 <juanjo8marzo@gmail.com>
2023-05-03 15:43:43 +0200

    Ejemplo 2.1 Profesor - Pintar rectángulos

--- Ejemplos_Clase/ejemplo2/codigo2.js
+++ Ejemplos_Clase/ejemplo2/codigo2.js
@@ -22,5 +22,22 @@ function ejemplo01() {
     ctx.moveTo(200, 300);
     ctx.lineTo(50, 280);
 
+    ctx.rect(20, 20, 100, 50); 
+
     ctx.stroke();
+}
+
+function ejemplo02() {
+    let cv = document.querySelector('#cv01');
+        ctx = cv.getContext('2d');
+
+    ctx.fillStyle = '#0a0';
+    ctx.fillRect(100,100, 200,150);
+    
+    ctx.strokeStyle = '#00a';
+    ctx.lineWidth = 20;
+
+    ctx.strokeRect(100,100, 200,150);
+
+    
 }
