828e0f1af367a38c3acd2bd43ce210c4ab3fa423
juanjo52 <juanjo8marzo@gmail.com>
2023-05-03 15:35:41 +0200

    Ejemplo 2 Profesor - Dibujar Líneas en canvas en distintas direcciones

---
+++ Ejemplos_Clase/ejemplo2/codigo2.js
@@ -0,0 +1,26 @@
+const ANCHO = 480;
+const ALTO = 360;
+
+function prepararCanvas() {
+    let cv = document.querySelector('#cv01');
+
+    cv.width = ANCHO;
+    cv.height = ALTO;
+}
+
+function ejemplo01() {
+    let cv = document.querySelector('#cv01');
+        ctx = cv.getContext('2d');
+    
+    ctx.strokeStyle = '#a00';
+    ctx.lineWidth = 2;
+
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 150);
+    ctx.lineTo(80, 200);
+
+    ctx.moveTo(200, 300);
+    ctx.lineTo(50, 280);
+
+    ctx.stroke();
+}
