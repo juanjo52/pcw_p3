63a1d4f6b0b85ce7d4347eef97d2ed8ad1757a7a
juanjo52 <juanjo8marzo@gmail.com>
2023-05-10 16:22:12 +0200

    Ejemplo 6.1 Profesor - Pintar imágenes

--- Ejemplos_Clase/ejemplo3/codigo3.js
+++ Ejemplos_Clase/ejemplo3/codigo3.js
@@ -2,10 +2,17 @@ const ANCHO = 480;
 const ALTO = 360;
 
 function prepararCanvas() {
-    let cv = document.querySelector('canvas');
+    // let cv = document.querySelector('canvas');
 
-    cv.width = ANCHO;
-    cv.height = ALTO;
+    // cv.width = ANCHO;
+    // cv.height = ALTO;
+    
+    let cvs = document.querySelectorAll('canvas');
+
+    cvs.forEach(function(cv){
+        cv.width = ANCHO;
+        cv.height = ALTO;
+    });
 }
 
 function ejemplo01() {
@@ -44,10 +51,55 @@ function escalar() {
     ctx.scale(1.25,1.25);
 }
 
-//Este está en apuntes (lo ha copiado y pegado y me da pereza xd es uno de rotaciones etc)
-function ejemplo02 {
+//Este está en apuntes (es uno de rotaciones etc)
+function ejemplo02 (){
     // let canvas = document.querySelector('#cv1'),
     //     ctx    = 
-
 }
 
+function imagen01() {
+    let cv = document.querySelector('#cv1'),
+        ctx = cv.getContext('2d'),
+        imagen,
+        x,y,
+        anchoImagen = cv.width * .8,
+        altoImagen;
+    
+    imagen = document.querySelector('img');
+
+    altoImagen = imagen.height * (anchoImagen / imagen.width);
+
+    x = (cv.width - anchoImagen) / 2;
+    y = (cv.height - altoImagen) / 2;
+
+    // ctx.drawImage(imagen, x, y, anchoImagen, altoImagen);
+    ctx.drawImage(imagen, 100, 100, 200, 150, 0,0, 200, 150);
+}
+
+function cargarImagen(inp) {
+    let file = inp.files[0];
+
+    if(file) {
+        let cv = document.querySelector('#cv1'),
+            ctx = cv.getContext('2d'),
+            img = new Image();
+        
+            img.onload = function() { // Para que de tiempo al cargar al ser asincrono ( lo que debimos hacer con el número de comentarios)
+
+                ctx.drawImage(img, 0,0, cv.width, img.height * (cv.width / img.width));
+            }
+
+            img.src = URL.createObjectURL( file );           
+    }
+}
+
+function selImagen(){ // Seleccionar imagen sin necesidad de tener input en el html... 
+    let inp = document.createElement('input');
+
+    inp.type = 'file';
+    inp.onchange = function(evt){
+        cargarImagen(evt.target);
+    }
+
+    inp.click();
+}
