0c549b6c7dc44e10bc31f60887a382fb1b4175d2
juanjo52 <juanjo8marzo@gmail.com>
2023-05-10 16:54:37 +0200

    Ejemplo Profesor 6.2  -  Copiar un canvas a otro y descargar imagen de canvas

--- Ejemplos_Clase/ejemplo3/codigo3.js
+++ Ejemplos_Clase/ejemplo3/codigo3.js
@@ -6,7 +6,7 @@ function prepararCanvas() {
 
     // cv.width = ANCHO;
     // cv.height = ALTO;
-    
+
     let cvs = document.querySelectorAll('canvas');
 
     cvs.forEach(function(cv){
@@ -102,4 +102,77 @@ function selImagen(){ // Seleccionar imagen sin necesidad de tener input en el h
     }
 
     inp.click();
+}
+
+function copiar() { // Copiar lo de un canvas en otro
+    let cv1 = document.querySelector('#cv1'),
+        ctx1 = cv1.getContext('2d'),
+        cv2 = document.querySelector('#cv2'),
+        ctx2 = cv2.getContext('2d');
+    
+    ctx2.drawImage(cv1, 0, 0);
+}
+
+function copiarID() {
+    let cv1 = document.querySelector('#cv1'),
+        ctx1 = cv1.getContext('2d'),
+        cv2 = document.querySelector('#cv2'),
+        ctx2 = cv2.getContext('2d'),
+        imgData;
+
+    
+    imgData = ctx1.getImageData(0,0, cv1.width, cv1.height);
+    
+    ctx2.putImageData(imgData, 0,0);
+}
+
+function aColor(color) {
+    let cv1 = document.querySelector('#cv1'),
+        ctx1 = cv1.getContext('2d'),
+        cv2 = document.querySelector('#cv2'),
+        ctx2 = cv2.getContext('2d'),
+        imgData, pixel;
+
+    
+    imgData = ctx1.getImageData(0,0, cv1.width, cv1.height);
+    
+    for(let i = 0; i < imgData.height; i++) {
+        for(let j = 0; j < imgData.width; j++) {
+            pixel = (i * imgData.width + j) * 4;
+
+            switch(color) {
+                case 'r':
+                    // imgData.data[ pixel    ] = 0;      // red
+                    imgData.data[ pixel + 1] = 0;   // green
+                    imgData.data[ pixel + 2] = 0;   // blue
+                    // imgData.data[ pixel + 3] = 0;   // alpha
+                    break;
+                case 'g':
+                    imgData.data[ pixel    ] = 0;      // red
+                    // imgData.data[ pixel + 1] = 0;   // green
+                    imgData.data[ pixel + 2] = 0;   // blue
+                    // imgData.data[ pixel + 3] = 0;   // alpha
+                    break;
+                case 'b':
+                    imgData.data[ pixel    ] = 0;      // red
+                    imgData.data[ pixel + 1] = 0;   // green
+                    // imgData.data[ pixel + 2] = 0;   // blue
+                    // imgData.data[ pixel + 3] = 0;   // alpha
+                    break;
+            }
+        }
+    }
+
+
+    ctx2.putImageData(imgData, 0,0);
+}
+
+function guardar() {
+    let cv1 = document.querySelector('#cv1'),
+        a = document.createElement('a');
+    
+    a.href = cv1.toDataURL('image/png');
+    a.download = 'download';
+
+    a.click();
 }
