e731a75f256f47262ce3155dfc0249dcf3624361
juanjo52 <juanjo8marzo@gmail.com>
2023-05-17 16:41:09 +0200

    Ejemplo 7.1 Profesor - editar elementos pulsados/arrastrados generados en el SVG (lo de arrastrar le ha petado al profe)

--- Ejemplos_Clase/ejemplo4/codigo4_2.js
+++ Ejemplos_Clase/ejemplo4/codigo4_2.js
@@ -22,8 +22,84 @@ function anyadir() {
                 stroke="#00a"
                 stroke-width = "6px"
                 fill="#a00"
+
+                onclick="seleccionar(event);"
+                onmousedown="clicDown(event);"
+                onmousemove="mover(event);"
+                onmouseup="clicUp(event);"
+                onmouseleave="mouseLeave(event);"
             />`;
 
     // Para ir añadiendo círculos al svg
     document.querySelector('#svg01').innerHTML += html;
+}
+
+function seleccionar(evt) {
+    let circle = evt.target,
+        sel = document.querySelector('.seleccionada');
+
+    // Para que solo se pueda seleccionar uno de los círculos
+    if(sel) {
+        sel.classList.remove('seleccionada');
+    }
+
+    circle.classList.add('seleccionada');
+
+    //Luego para pillar todos los seleccionador ponermos document.querySelectorAll('.seleccionada') 
+
+    //Si tiene la clase se la quita y si no se la pone asi:  
+    // circle.classList.toggle('seleccionada');
+
+}
+
+
+function clicDown(evt) {
+    let circle = evt.target,
+        x = evt.offsetX,
+        y = evt.offsetY,
+        pos;
+
+    pos = {'x':x, 'y':y};
+
+    circle.setAttribute('data-pos', JSON.stringify(pos));
+
+    console.log('DOWN');
+    console.log(circle);
+}
+
+function clicUp(evt) {
+    let circle = evt.target;
+
+    if(circle.getAttribute('data-pos')){
+        circle.removeAttribute('data-pos')
+    }
+
+    console.log('UP');
+    console.log(circle);
+}
+
+function mover(evt) {
+    let circle = evt.target,
+        x = evt.offsetX,
+        y = evt.offsetY;
+
+    if(circle.getAttribute('data-pos')){
+        let pos = JSON.parse(circle.getAttribute('data-pos')),
+            dx = parseInt(x - pos.x),
+            dy = parseInt(y - pos.y),
+            cx, cy;
+
+            cx = parseInt(circle.getAttribute('cx')) + parseInt(dx);
+            cy = parseInt(circle.getAttribute('cy')) + parseInt(dy);
+
+            circle.setAttribute('cx', cx);
+            circle.setAttribute('cy', cy);
+
+            pos = {'x':x, 'y':y};
+            circle.setAttribute('data-pos', JSON.stringify(pos));
+    }
+}
+
+function mouseLeave(evt) {
+    clicUp(evt);
 }
